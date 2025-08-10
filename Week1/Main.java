2bd290873b3b450f9a445a4340ed70a9ba89e35a
Iris Sen <irissen8@gmail.com>
2025-08-10 12:09:17 +0530

    initial comit

---
+++ Week1/Main.java
@@ -0,0 +1,72 @@
+/*public class Main {
+ public static void main(String[] args) {
+   System.out.println("Hello, World!");
+ }
+}  */
+
+/*public class Main {
+  public void greet() {
+    System.out.println("Hello");
+  }
+
+  public static void main(String[] args) {
+    Main obj = new Main();
+    obj.greet();
+  }
+} */
+
+/*public class Main {
+  public static void main(String[] args) {
+    String number = "10";
+    System.out.println("The number is: " + number);
+  }
+} */
+
+/*public class Main {
+  public static void main(String[] args) {
+    int[] numbers = { 1, 2, 3, 4 };
+    System.out.println("The fourth element is: " + numbers[3]);
+  }
+} */
+
+/*public class Main {
+  public static void main(String[] args) {
+    int result = addNumbers(5, 10);
+    System.out.println("Result: " + result);
+  }
+
+  public static int addNumbers(int a, int b) {
+    return a + b;
+  }
+} */
+
+/*public class Main {
+  public static void main(String[] args) {
+    int age = 20;
+    if (age >= 18) {
+      System.out.println("You are eligible to vote.");
+    }
+  }
+} */
+
+/*public class Main {
+  public static void main(String[] args) {
+    int i;
+    for (i = 0; i < 5; i++) {
+      System.out.println("Number: " + i);
+    }
+    i++;
+    System.out.println("Outside loop: " + i);
+  }
+} */
+
+public class Main {
+  public static void main(String[] args) {
+    int count = 0; // declare and initialize
+
+    while (count < 10) {
+      System.out.println("Count: " + count);
+      count++;
+    }
+  }
+}
---
+++ Week1/code2.java
@@ -0,0 +1,10 @@
+public class Main {
+  public void greet() {
+    System.out.println("Hello");
+  }
+
+  public static void main(String[] args) {
+    Main obj = new Main();
+    obj.greet();
+  }
+}
